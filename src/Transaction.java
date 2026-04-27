2b0efb7b97385f72dc86e4cf1ed2303657c7f6d7
Ayush Sadavarte <ayushsadavarte2024.comp@mmcoe.edu.in>
2026-04-27 20:02:45 +0530

    Change accountId type from String to int

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,31 +1,27 @@
-// Transaction: Stores details of each banking transaction
 public class Transaction {
 
-    private String type;       // "DEPOSIT", "WITHDRAW", or "TRANSFER"
+    private String type;
     private double amount;
-    private String accountId;
+    private int accountId;
     private String date;
 
-    public Transaction(String type, double amount, String accountId, String date) {
+    public Transaction(String type, double amount, int accountId, String date) {
         this.type = type;
         this.amount = amount;
         this.accountId = accountId;
         this.date = date;
     }
 
-    // Returns a simple CSV line for file storage
     public String toCSV() {
         return accountId + "," + type + "," + amount + "," + date;
     }
 
-    // Returns a readable summary for display
     @Override
     public String toString() {
         return "[" + date + "] " + type + " of Rs." + amount + " on Account: " + accountId;
     }
 
-    // Getters
-    public String getAccountId() { return accountId; }
+    public int getAccountId() { return accountId; }
     public String getType()      { return type; }
     public double getAmount()    { return amount; }
 }
