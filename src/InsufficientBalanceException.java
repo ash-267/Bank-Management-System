26678ecf178a7e93e599af2b3d9fd084793f413c
Ayush Sadavarte <ayushsadavarte2024.comp@mmcoe.edu.in>
2026-04-27 20:03:11 +0530

    Remove comment from InsufficientBalanceException
    Removed comment explaining the custom exception.

--- src/InsufficientBalanceException.java
+++ src/InsufficientBalanceException.java
@@ -1,4 +1,3 @@
-// Custom Exception: Thrown when withdrawal amount exceeds account balance
 public class InsufficientBalanceException extends Exception {
     public InsufficientBalanceException(String message) {
         super(message);
